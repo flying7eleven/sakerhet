6f6380eecb37d9d85fb13ecc878f6572926a19a8
Tim Hütz <tim@huetz.biz>
2019-10-02 19:01:52 +0200

    feature: add an option to show a valid configuration to the user

---
+++ src/configuration/mod.rs
@@ -0,0 +1,34 @@
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::string::ToString;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Configuration {
+    #[serde(default)]
+    pub observed_directories: Vec<String>,
+}
+
+impl Default for Configuration {
+    fn default() -> Self {
+        Configuration {
+            observed_directories: vec!["/boot".to_string(), "/usr/bin".to_string()],
+        }
+    }
+}
+
+impl Configuration {
+    pub fn from_file(config_file: &str) -> Configuration {
+        let file_handle = File::open(config_file);
+        if file_handle.is_ok() {
+            let read_configuration: Configuration =
+                serde_yaml::from_reader(file_handle.unwrap()).unwrap();
+            return read_configuration;
+        }
+        Configuration::default()
+    }
+
+    pub fn from_yaml(configuration: &str) -> Configuration {
+        let read_configuration: Configuration = serde_yaml::from_str(configuration).unwrap();
+        read_configuration
+    }
+}
